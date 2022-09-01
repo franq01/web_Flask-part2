15135f5e93c4f73be27b19df8fc5e98424cdf595
franq01 <carreone705@gmail.com>
2022-09-01 15:39:32 -0500

    5_1 App Projects

---
+++ web_personal/projects/views.py
@@ -0,0 +1,19 @@
+################# Import de Flask #####################
+from flask import render_template, request, Blueprint
+
+project_blueprint = Blueprint('project', __name__)
+
+########################### TODO: List project ##################
+
+
+########################### TODO: create a project ##################
+
+
+########################### TODO: update a project ##################
+
+
+
+########################### TODO: delete project ##################
+
+
+
