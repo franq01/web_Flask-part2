c7d3f5d2958b1c527506e00719ee3ab6a624f628
franq01 <carreone705@gmail.com>
2022-09-01 16:33:28 -0500

    5_2 Rutas y templates iniciales minuto 8:12

--- web_personal/app.py
+++ web_personal/app.py
@@ -6,6 +6,7 @@ from flask import Flask
 from home.views import home_blueprint
 from auth.views import auth_blueprint
 from error_pages.handlers import error_pages
+from projects.views import project_blueprint
 
 from db.db_conection import get_conection
 
@@ -20,6 +21,7 @@ app.config['ENV']= 'developmen'
 app.register_blueprint(home_blueprint)
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 app.register_blueprint(error_pages)
+app.register_blueprint(project_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- web_personal/projects/views.py
+++ web_personal/projects/views.py
@@ -6,8 +6,21 @@ project_blueprint = Blueprint('project', __name__)
 ########################### TODO: List project ##################
 
 
-########################### TODO: create a project ##################
+@project_blueprint.route('/')
+def list():
+    return render_template('project/list.html')
 
+
+########################### TODO: Show project ##################
+@project_blueprint.route('/<project_id>')
+def show(project_id):
+    return render_template('project/show.html', project_id=project_id)
+
+
+########################### TODO: create a project ##################
+@project_blueprint.route('/new')
+def new():
+    return render_template('project/new.htm')
 
 ########################### TODO: update a project ##################
 
